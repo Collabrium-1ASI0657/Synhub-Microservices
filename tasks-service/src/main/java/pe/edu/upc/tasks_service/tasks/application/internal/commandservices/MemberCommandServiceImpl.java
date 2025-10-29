8eee90fb69f7edbdc8b46c1409f262406ddaa432
Miguel26112001 <u202220294@upc.edu.pe>
2025-10-28 23:08:30 -0500

    feature(tasks): SECOND project code

--- tasks-service/src/main/java/pe/edu/upc/tasks_service/tasks/application/internal/commandservices/MemberCommandServiceImpl.java
+++ tasks-service/src/main/java/pe/edu/upc/tasks_service/tasks/application/internal/commandservices/MemberCommandServiceImpl.java
@@ -1,4 +1,31 @@
 package pe.edu.upc.tasks_service.tasks.application.internal.commandservices;
 
-public class MemberCommandServiceImpl {
+import org.springframework.stereotype.Service;
+import pe.edu.upc.tasks_service.tasks.domain.model.aggregates.Member;
+import pe.edu.upc.tasks_service.tasks.domain.model.commands.AddGroupToMemberCommand;
+import pe.edu.upc.tasks_service.tasks.domain.model.commands.CreateMemberCommand;
+import pe.edu.upc.tasks_service.tasks.domain.services.MemberCommandService;
+import pe.edu.upc.tasks_service.tasks.infrastructure.persistence.jpa.repositories.MemberRepository;
+
+import java.util.Optional;
+
+@Service
+public class MemberCommandServiceImpl implements MemberCommandService {
+  private final MemberRepository memberRepository;
+
+  public MemberCommandServiceImpl(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
+  }
+
+  @Override
+  public Optional<Member> handle(CreateMemberCommand command) {
+    var member = new Member(command);
+    var createdMember = memberRepository.save(member);
+    return Optional.of(createdMember);
+  }
+
+  @Override
+  public Optional<Member> handle(AddGroupToMemberCommand command) {
+    return Optional.empty();
+  }
 }
--- tasks-service/src/main/java/pe/edu/upc/tasks_service/tasks/application/internal/queryservices/MemberQueryServiceImpl.java
+++ tasks-service/src/main/java/pe/edu/upc/tasks_service/tasks/application/internal/queryservices/MemberQueryServiceImpl.java
@@ -1,4 +1,43 @@
 package pe.edu.upc.tasks_service.tasks.application.internal.queryservices;
 
-public class MemberQueryServiceImpl {
+import org.springframework.stereotype.Service;
+import pe.edu.upc.tasks_service.tasks.domain.model.aggregates.Member;
+import pe.edu.upc.tasks_service.tasks.domain.model.queries.GetAllMembersQuery;
+import pe.edu.upc.tasks_service.tasks.domain.model.queries.GetMemberByIdQuery;
+import pe.edu.upc.tasks_service.tasks.domain.model.queries.GetMemberByUsernameQuery;
+import pe.edu.upc.tasks_service.tasks.domain.model.queries.GetMembersByGroupIdQuery;
+import pe.edu.upc.tasks_service.tasks.domain.model.valueobjects.GroupId;
+import pe.edu.upc.tasks_service.tasks.domain.services.MemberQueryService;
+import pe.edu.upc.tasks_service.tasks.infrastructure.persistence.jpa.repositories.MemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberQueryServiceImpl implements MemberQueryService {
+  private final MemberRepository memberRepository;
+
+  public MemberQueryServiceImpl(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
+  }
+
+  @Override
+  public Optional<Member> handle(GetMemberByIdQuery query) {
+    return memberRepository.findById(query.memberId());
+  }
+
+  @Override
+  public Optional<Member> handle(GetMemberByUsernameQuery query) {
+    return Optional.empty();
+  }
+
+  @Override
+  public List<Member> handle(GetAllMembersQuery query) {
+    return this.memberRepository.findAll();
+  }
+
+  @Override
+  public List<Member> handle(GetMembersByGroupIdQuery query) {
+    return memberRepository.findMembersByGroupId(new GroupId(query.groupId()));
+  }
 }
