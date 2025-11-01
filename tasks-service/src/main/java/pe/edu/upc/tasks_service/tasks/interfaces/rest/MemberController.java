8cb21fa02b8bae7447e5c9b86d6199de9edeb385
Miguel26112001 <u202220294@upc.edu.pe>
2025-10-31 20:21:44 -0500

    feature(tasks): added get member by authentication

--- tasks-service/src/main/java/pe/edu/upc/tasks_service/tasks/interfaces/rest/MemberController.java
+++ tasks-service/src/main/java/pe/edu/upc/tasks_service/tasks/interfaces/rest/MemberController.java
@@ -3,10 +3,10 @@ package pe.edu.upc.tasks_service.tasks.interfaces.rest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.tasks_service.tasks.domain.model.queries.GetMemberByIdQuery;
+import pe.edu.upc.tasks_service.tasks.domain.model.queries.GetMemberByUsernameQuery;
 import pe.edu.upc.tasks_service.tasks.domain.model.queries.GetMemberInfoByIdQuery;
 import pe.edu.upc.tasks_service.tasks.domain.services.MemberQueryService;
 import pe.edu.upc.tasks_service.tasks.interfaces.rest.resources.MemberOnlyResource;
@@ -23,6 +23,21 @@ public class MemberController {
   public MemberController(MemberQueryService memberQueryService) {
     this.memberQueryService = memberQueryService;
   }
+
+  @GetMapping("/details")
+  @Operation(summary = "Get member details by authentication", description = "Fetches the details of the authenticated member.")
+  public ResponseEntity<MemberResource> getMemberByAuthentication(@RequestHeader("Authorization") String authorizationHeader,
+                                                                  @RequestHeader("X-Username") String username) {
+    var getMemberByUsernameQuery = new GetMemberByUsernameQuery(username, authorizationHeader);
+    var memberWithUserInfo = this.memberQueryService.handle(getMemberByUsernameQuery);
+    if (memberWithUserInfo.isEmpty()) return ResponseEntity.notFound().build();
+
+    var memberResource = MemberResourceFromEntityAssembler
+        .toResourceFromUserResource(memberWithUserInfo.get(), memberWithUserInfo.get().member().id());
+
+    return ResponseEntity.ok(memberResource);
+  }
+
 
   @GetMapping("/details/{memberId}")
   @Operation(summary = "Get member details by member ID", description = "Fetches the details of a member by their ID.")
