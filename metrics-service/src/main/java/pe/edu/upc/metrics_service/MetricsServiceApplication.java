d7ff4b20d732149bd65ad86fdb859a0bee0c8b88
Pache <54517063+Pachieeee@users.noreply.github.com>
2025-12-04 17:39:59 -0500

    fix(metrics): exclude DataSource and Hibernate JPA auto-configuration in MetricsServiceApplication.

--- metrics-service/src/main/java/pe/edu/upc/metrics_service/MetricsServiceApplication.java
+++ metrics-service/src/main/java/pe/edu/upc/metrics_service/MetricsServiceApplication.java
@@ -2,11 +2,15 @@ package pe.edu.upc.metrics_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-@EnableJpaAuditing
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 @EnableScheduling
 public class MetricsServiceApplication {
 
